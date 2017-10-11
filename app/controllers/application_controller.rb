7a63d898501c884fad4eb06ab731b66fc40ad8f7
SimoDimitrov <simo.dimitrov.inc@gmail.com>
2017-10-11 11:56:48 +0300

    neshichko

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,48 @@
+require 'csv'
+require 'linear-regression'
+class ApplicationController < ActionController::Base
+protect_from_forgery with: :null_sessions
+
+def sums
+	CSV.foreach do |row|
+	sum=0
+	sum += row[0]
+	sum=sum.ceil
+	render plain: ('%.2f'%sum)
+end
+
+def filter
+	CSV.foreach do |row|
+	sum=0
+	sum+erow[1] if row[2]%2!=0
+	  end
+	end	
+	sum=sum.ceil
+	render plain: ('%.2f'%sum)
+end
+
+
+def intervals
+    sum=0
+    line=1
+    result=0
+    file=CSV.parse(params[:file].read)
+    while line < file.length-30 do
+      i=0
+      while i<30
+        sum+=file[line+i][0].to_i
+        i+=1
+      end  
+      if sum > result
+        result=sum
+      end 
+      sum=0
+      line+=1  
+   end
+   result=result.ceil
+   render plain: ('%.2f' % result )
+
+  end 
+
+
+end
